5eafe4e01dac999b0dca5a840857b1d77ed2e2c0
Edgar-MedCor <edgarmen159@gmail.com>
2026-05-07 16:17:41 -0500

    se cambio ulr para pruebas desde vercel

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://crawling-evasion-freehand.ngrok-free.dev',
         changeOrigin: true
       }
     }
